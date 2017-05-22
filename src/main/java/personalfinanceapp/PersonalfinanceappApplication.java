a886e18f49f7673a80ff2613c5b686022b015412
saminhajru <saminhajru@gmail.com>
2017-05-22 22:40:35 +0200

    Initial commit

---
+++ src/main/java/personalfinanceapp/PersonalfinanceappApplication.java
@@ -0,0 +1,12 @@
+package personalfinanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinanceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinanceappApplication.class, args);
+	}
+}
---
+++ src/test/java/personalfinanceapp/PersonalfinanceappApplicationTests.java
@@ -0,0 +1,16 @@
+package personalfinanceapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalfinanceappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
