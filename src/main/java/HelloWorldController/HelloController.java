1b826d2cebaa04370f6d8dc77b34f47cd6342cc0
saminhajru <saminhajru@gmail.com>
2017-05-22 22:54:32 +0200

    Hello World Controller

---
+++ src/main/java/HelloWorldController/HelloController.java
@@ -0,0 +1,14 @@
+package HelloWorldController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String Hello() {
+		return "hello";
+	}
+
+}
