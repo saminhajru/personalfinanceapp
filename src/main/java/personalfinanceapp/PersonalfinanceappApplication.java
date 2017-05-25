c52d828540921abe829fff9af9b70d118551a509
saminhajru <saminhajru@gmail.com>
2017-05-25 22:43:23 +0200

    Registration with password encoder

--- src/main/java/personalfinanceapp/PersonalfinanceappApplication.java
+++ src/main/java/personalfinanceapp/PersonalfinanceappApplication.java
@@ -2,9 +2,6 @@ package personalfinanceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @SpringBootApplication
 public class PersonalfinanceappApplication {
--- src/main/java/personalfinanceapp/controller/HelloWorldController.java
+++ src/main/java/personalfinanceapp/controller/HelloWorldController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloWorldController {
 
-	@RequestMapping(value = "/hello", method= RequestMethod.GET)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
 		return "helloWorld";
 	}
 	
-	@RequestMapping(value = "/home", method= RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
 		return "home";
 	}
