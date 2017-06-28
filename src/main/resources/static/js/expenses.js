cb57cbf78f2e13edfd15c7123270fec060f6e955
saminhajru <saminhajru@gmail.com>
2017-06-28 20:02:52 +0200

    Added a sort plugin to the expenses display table

--- src/main/resources/static/js/expenses.js
+++ src/main/resources/static/js/expenses.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#categoryRow").on("change", function() {
+	$("#categoryRow").on("click change", function() {
 
 		$("#subcategoryRow").empty();
 
@@ -55,7 +55,7 @@ $(document).ready(function() {
 				timeout: 600000,
 			
 				success : function(data) {
-					$("#tableForDisplayExpensesBodyContainer table").append(data);
+					$("#tableForDisplaying tbody").append(data);
 					$("#modalForAddingExpense").modal("hide");
 				},
 				error : function() {
@@ -133,8 +133,8 @@ $(document).ready(function() {
 				timeout: 600000,
 			
 				success : function(data) {
-					$("#tableForDisplaying").empty();
-					$("#tableForDisplaying").append(data);
+					$("#tableForDisplaying tbody").empty();
+					$("#tableForDisplaying tbody").append(data);
 				},
 				error : function() {
 					alert("Error");
