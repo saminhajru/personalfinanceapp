b7feb3abf21b6e6219d6e953a6e31d6bd8ea5d0b
saminhajru <saminhajru@gmail.com>
2017-07-27 11:15:38 +0200

    Configured log4j2

--- src/main/java/personalfinanceapp/controller/HomeController.java
+++ src/main/java/personalfinanceapp/controller/HomeController.java
@@ -1,5 +1,9 @@
 package personalfinanceapp.controller;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static Logger logger = LogManager.getLogger(HomeController.class);
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeLogged() {
+		logger.error("Hello");
 		return "home";
 	}
 	
